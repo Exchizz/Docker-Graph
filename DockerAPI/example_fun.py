648e720c7437858c54a16feb273b6e3a49516556
Mathias Neerup <manee12@student.sdu.dk>
2016-11-10 14:33:39 +0100

    Lists interfaces and the docker machines connected

--- DockerAPI/example_fun.py
+++ DockerAPI/example_fun.py
@@ -1,14 +1,22 @@
+from __future__ import print_function
 from docker import Client
-
+from pprint import pprint
 cli = Client(base_url='unix://var/run/docker.sock')
 
 #print cli.containers()[0]
 
 
-networks = cli.networks()[0]
-
-#print networks
-
-inspect_network = cli.inspect_network("faf69dd0b88d28d717af80b70b0d805ff78814c3f5661c83f9976e61bec8d157")
-
-print inspect_network
+networks = cli.networks()
+for network in networks:
+	id = network['Id']
+	network_ = network['IPAM']['Config']
+	print(network['Driver'], " ", end="")
+	try:
+		print(network_[0]['Subnet'])
+	except:
+		print("No dhcp")
+	inspect_network = cli.inspect_network(id)
+	for host in inspect_network['Containers']:
+		docker = inspect_network["Containers"][host]
+		print("\tFound docker: ", docker["Name"], " ip: ", docker["IPv4Address"])
+		
